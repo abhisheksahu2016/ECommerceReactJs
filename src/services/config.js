9c958cd62150ba480dccf5b29ec81cd2b90674cf
Abhishek Sahu <imabhisheksahu2016@gmail.com>
2024-10-20 16:17:58 +0530

    first commit

---
+++ src/services/config.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+  // apiKey: process.env.FIREBASE_API_KEY,
+  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.FIREBASE_DB_URL,
+  // projectId: process.env.FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
+  // appId: process.env.FIREBASE_APP_ID
+
+  apiKey: "AIzaSyAS2vXSvHH6qJYR8bANJ9DiOCcHsfoohuA",
+    authDomain: "unique-perigee-302408.firebaseapp.com",
+    projectId: "unique-perigee-302408",
+    storageBucket: "unique-perigee-302408.appspot.com",
+    messagingSenderId: "180522197820",
+    appId: "1:180522197820:web:b2f46b501461f6b9a53439"
+};
+
+export default firebaseConfig;
